f3048bbad9778f836227987faa81169da5daf771
wangzhongshu <wzscq@126.com>
2022-05-13 11:32:43 +0800

    Update index.js

--- src/pages/View/ListTable/ColumnControl/index.js
+++ src/pages/View/ListTable/ColumnControl/index.js
@@ -1,7 +1,6 @@
 import { FIELD_TYPE } from "../../../../utils/constant";
 
 export default function ColumnControl({text,field, record, index}){
-    console.log('field',field);
     let value=text;
     if(text&&field.fieldType===FIELD_TYPE.MANY2ONE){
         if(field.optionLabel&&text.list&&text.list.length>0){
